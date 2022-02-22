1cf1892fa62aa5361e2247d8b31f4eddbbbb615e
elicodes89 <elicodes89@gmail.com>
2022-02-22 21:58:45 +0000

    hwork C almost complete. 2,3,4 are missing

---
+++ hwork_c.py
@@ -0,0 +1,31 @@
+united_kingdom = [
+  {
+    "name": "Scotland",
+    "population": 5295000,
+    "capital": "Edinburgh"
+  },
+  {
+    "name": "Wales",
+    "population": 3063000,
+    "capital": "Swansea"
+  },
+  {
+    "name": "England",
+    "population": 53010000,
+    "capital": "London"
+  }
+]
+
+united_kingdom[1]['capital'] = "Cardiff"
+print(united_kingdom[1]["capital"])
+
+# northern_ireland_dic = [
+
+#     {
+
+#     "population": "1,811,000" ,
+#     "capital": "Belfast"
+#   }
+# ]
+
+
